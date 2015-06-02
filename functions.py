ed46aa4fc9a3e6cf605092240157672ea5a7f838
root <max.scribs@gmail.com>
2015-06-02 17:01:57 -0400

    integrated nesting in lispish interpreter

--- functions.py
+++ functions.py
@@ -27,22 +27,42 @@ def interpret_block(simple_lisp):
 			left division in because I want to work with integers for the time being
 		(x m n) does m to the power of n
 		(l m n) does log m base n
-
-		will allow nesting later
 	'''
-	end_m = simple_lisp.find(" ",3)
-	m = int(simple_lisp[3:end_m])
-	n = int(simple_lisp[end_m + 1:-1])
+	if simple_lisp.count("(") == simple_lisp.count(")"):
+		
+		end_m = get_end_of_block(simple_lisp[3:])
+		m = simple_lisp[3:end_m+4]
+		n = simple_lisp[end_m+5:-1]
 
-	if simple_lisp[1]== '+':
-		return m + n
-	elif simple_lisp[1]== '*':
-		return m * n		
-	elif simple_lisp[1]== '/':
-		return m / n
-	elif simple_lisp[1]== 'x':
-		return m ** n
-	elif simple_lisp[1]== 'l':
-		return log(m, n)
-	else:
-		return "Invalid Structure"
+		if m[0] == "(":
+			m = interpret_block(m)
+		if n[0] == "(":
+			n = interpret_block(n)
+
+		if simple_lisp[1] in ['+', '*', '/', '^', 'l']:
+			m = float(m)
+			n = float(n)
+			if simple_lisp[1]== '+':
+				return m + n
+			elif simple_lisp[1]== '*':
+				return m * n		
+			elif simple_lisp[1]== '/':
+				return m / n
+			elif simple_lisp[1]== '^':
+				return m ** n
+			elif simple_lisp[1]== 'l':
+				return log(m, n)
+		else:
+			return "Err:Operand"
+	return "Err:Unbalanced Parentheses"
+
+def get_end_of_block(simple_lisp):
+	parenthesis = 0
+	for i in range(len(simple_lisp)):
+		if simple_lisp[i] == '(':
+			parenthesis += 1
+		if simple_lisp[i] == ')':
+			parenthesis -= 1
+
+		if parenthesis == 0:
+			return i
---
+++ test.py
@@ -0,0 +1,3 @@
+import functions
+
+print(functions.interpret_block("(+ (^ 2 3) (* 3 5))"))
